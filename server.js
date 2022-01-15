80fcb633d80054af5f5beaf62a5b37da1422034f
Kakashi <nickperel@live.com>
2022-01-14 21:23:39 -0500

    created HTML routes for notes.html and index.html, created API routes for GET & POST requests, installed express and set up server, installed and implemented uniqid to give each note a unique id as it is created, used express.static for css implementation

--- server.js
+++ server.js
@@ -1,12 +1,40 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+const db_notes = require('./db/db.json');
+const uniqid = require('uniqid');
 const app = express();
 const PORT = 3001;
 
+
 app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
 
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+        if (err) throw err;
+        res.json(JSON.parse(data));
+    });
+});
+
+app.post('/api/notes', (req, res) => {
+    const data = {
+        id: uniqid(),
+        title: req.body.title,
+        text: req.body.text
+    };
+    console.log(data);
+    db_notes.push(data);
+    fs.writeFile('./db/db.json', JSON.stringify(db_notes), () => {
+        res.send('Success');
+    });
 });
 
 app.get('*', (req, res) => {
